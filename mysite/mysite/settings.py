f7dcaf0d30da02c683d61db5b8144333b66560f8
guru1112001 <107809909+guru1112001@users.noreply.github.com>
2022-07-23 19:44:55 +0530

    add a profile section

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT= os.path.join(BASE_DIR,"media")
+MEDIA_URL="/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -127,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATES_PACK="bootstrap4"
 LOGIN_REDIRECT_URL= "blog-home"
 LOGIN_URL="login"
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,20 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from tempfile import template
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_veiws
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
+    path('',include('blog.urls')),
     path("register/",user_veiws.resgister,name="register"),
     path("profile/", user_veiws.profile,name="profile"),
     path("login/",auth_views.LoginView.as_view(template_name="users/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(template_name="users/logout.html"),name="logout"),
 
-
-    
-
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
