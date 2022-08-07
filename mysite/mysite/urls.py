7d8913edc6868725bf8fbe8ed758e4a6c33699c4
guru1112001 <107809909+guru1112001@users.noreply.github.com>
2022-08-07 23:58:34 +0530

    fix the issue in url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path("profile/", user_veiws.profile,name="profile"),
     path("login/",auth_views.LoginView.as_view(template_name="users/login.html"),name="login"),
     path("logout/",auth_views.LogoutView.as_view(template_name="users/logout.html"),name="logout"),
+    path("logout/",auth_views.LogoutView.as_view(template_name="users/logout.html"),name="logout"),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
