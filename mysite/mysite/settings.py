e34237ac2b691e47e2722ff6f1d43c38e56f5a8a
guru1112001 <107809909+guru1112001@users.noreply.github.com>
2022-08-08 01:35:28 +0530

    added password reset feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,4 +129,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATES_PACK="bootstrap4"
 LOGIN_REDIRECT_URL= "blog-home"
 LOGIN_URL="login"
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_TLS=True
+EMAIL_HOST_USER="gbhtia266@gmail.com"
+EMAIL_HOST_USER="etzhdhnecddgwofr"
+
 
